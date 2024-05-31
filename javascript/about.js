8ce525a116a9724006923c3dd6629d31ae662a88
John Kahookele <kahookelekelii@gmail.com>
2024-05-30 21:39:59 -1000

    expanded pictures

--- javascript/about.js
+++ javascript/about.js
@@ -11,8 +11,30 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   };
 
+  function showPics() {
+    const pics = document.querySelectorAll('.img');
+
+    pics.forEach(pic => {
+      pic.addEventListener('click', (event) => {
+        // Prevent click event from propagating to document
+        event.stopPropagation();
+
+        // Toggle the 'expanded' class
+        pic.classList.toggle('expanded');
+      });
+    });
+
+    // Add an event listener to the document to reset styles when clicking anywhere else
+    document.addEventListener('click', () => {
+      pics.forEach(pic => {
+        pic.classList.remove('expanded');
+      });
+    });
+  }
+
   window.addEventListener('resize', resizeText);
 
   // Call the function initially to set the size on page load
   resizeText();
+  showPics();
 });
